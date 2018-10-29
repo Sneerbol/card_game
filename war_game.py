ef2870d3edfc124676e5b897432c05df2c0863da
Sneerbol <romas.m@mail.ru>
2018-10-29 23:03:40 +0200

    Added one more class and fixed some bugs after debug :)

--- war_game.py
+++ war_game.py
@@ -16,7 +16,7 @@ class Deck:
     """
     def __init__(self):
         print("Creating New Ordered Deck!")
-        self.allcards = [(s,r) for s Suite for r in Ranks ]
+        self.allcards = [(s,r) for s in Suite for r in Ranks]
 
     def shuffle(self):
         print("Shuffling deck")
@@ -31,7 +31,7 @@ class Hand:
     This is the Hand Class. Each player has a Hand, and can add or remove cards from that hand. There should be an add and remove card method here.
 
     """
-    def __init(self, cards):
+    def __init__(self, cards):
         self.cards = cards
 
     def __str__(self):
@@ -56,5 +56,131 @@ class Player:
     def play_card(self):
 
         drawn_card = self.hand.remove_card()
+        print("{} has placed: {}".format(self.name,drawn_card))
+        print("\n")
+        return drawn_card
+    
+    def remove_war_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.cards.pop())
+        
+        return war_cards
+
+    def still_has_cards(self):
+
+        return len(self.hand.cards) != 0
+
+
+    # GAME PLAY #
+
+print("Welcome to War, let's begin...")
+
+
+#create new deck and split it in half
+
+d = Deck()
+d.shuffle()
+half1,half2 = d.split_in_half()
+
+#create both players
+computer = "computer"
+comp = Player(computer, Hand(half1))
+name = "Gamer"
+user = Player(name,Hand(half2))
+
+total_rounds = 0
+war_count = 0
+
+while user.still_has_cards() and comp.still_has_cards():
+    total_rounds += 1
+    print("Time for a new round")
+    print("here are the current stanings")
+    print(user.name+"has the count: "+str(len(user.hand.cards)))
+    print(comp.name+"has the count: "+str(len(comp.hand.cards)))
+    print("play a card!")
+    print("\n")
+
+    table_cards = []
+    c_card = comp.play_card()
+    p_card = user.play_card()
+    table_cards.append(c_card)
+    table_cards.append(p_card)
+
+    if c_card[1] == p_card[1]:
+        war_count += 1
+        print("war!")
+
+        table_cards.extend(user.remove_war_cards())
+        table_cards.extend(comp.remove_war_cards())
+
+        if Ranks.index(c_card[1]) < Ranks.index(p_card[1]):
+            user.hand.add(table_cards)
+        else:
+            comp.hand.add(table_cards) 
+    else:
+        if Ranks.index(c_card[1]) < Ranks.index(p_card[1]):
+            user.hand.add(table_cards)
+
+        else:
+            comp.hand.add(table_cards)
+
+
+print("Game over, number of round:"+str(total_rounds))
+print("a war happened "+str(war_count)+"times")
+if str(comp.still_has_cards()) == "True" and str(user.still_has_cards()) == "False":
+    print("Computer won")
+else:
+    print("User won")
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
 
         
